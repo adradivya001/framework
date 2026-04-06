74a7c5566cbb9408dd33487acbd32a13fa50baae
adradivya001 <adradivya001@github.com>
2026-04-06 14:33:53 +0530

    chore: Final cleanup of deprecated legacy folders and synchronization of production types

--- _DELETING_JS_Clinics_Backend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Required for Docker deployment - creates standalone build
-  // output: 'standalone',
-};
-
-module.exports = nextConfig;
